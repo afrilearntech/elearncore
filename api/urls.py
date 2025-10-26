ad3ef446bfad17f0182376b1e7b489739126a6d9
princek <princesamuelpks@gmail>
2025-10-26 20:21:21 +0000

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .viewsets import *
+
+# from .viewsets import *  # ViewSets can be registered to the router below when available
+
+router = DefaultRouter()
+
+urlpatterns = [
+	path('', include(router.urls)),
+]
