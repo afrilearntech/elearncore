9f8025f060d9963585d7ae3c1c975704c26edf34
princek <princesamuelpks@gmail>
2025-10-26 20:21:52 +0000

    Update admin.py

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Forum, ForumMembership, Chat
+
+
+@admin.register(Forum)
+class ForumAdmin(admin.ModelAdmin):
+	list_display = ("id", "name", "created_at")
+	search_fields = ("name",)
+
+
+@admin.register(ForumMembership)
+class ForumMembershipAdmin(admin.ModelAdmin):
+	list_display = ("id", "forum", "student", "created_at")
+	list_filter = ("forum",)
+
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+	list_display = ("id", "sender", "forum", "created_at")
+	list_filter = ("forum",)
+	search_fields = ("sender__name", "content")
