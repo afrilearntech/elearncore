165c5f9f6469aa80d5b12db019e65e62484f08c1
princek <princesamuelpks@gmail>
2025-10-26 20:21:05 +0000

    Update admin.py

--- agentic/admin.py
+++ agentic/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import AIRecommendation, AIAbuseReport
+
+
+@admin.register(AIRecommendation)
+class AIRecommendationAdmin(admin.ModelAdmin):
+	list_display = ("id", "student", "lesson", "created_at")
+	list_filter = ("lesson",)
+
+
+@admin.register(AIAbuseReport)
+class AIAbuseReportAdmin(admin.ModelAdmin):
+	list_display = ("id", "tag", "forum", "created_at")
+	list_filter = ("forum",)
+	search_fields = ("tag", "description")
