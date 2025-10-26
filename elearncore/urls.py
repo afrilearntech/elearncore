78f5b9d8fc994d7c64568a1bd5ea7d4a5be53eca
princek <princesamuelpks@gmail>
2025-10-26 19:35:46 +0000

    Update urls.py

--- elearncore/urls.py
+++ elearncore/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # api path
+    path('api-v1/', include('api.urls')),
 ]
