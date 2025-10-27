829a7714f77622cb70cc1537ddee6b9cae0c30bf
princek <princesamuelpks@gmail>
2025-10-27 09:26:24 +0000

    Cache schema endpoint for 1 hour
    Applied Django's cache_page decorator to the 'api-v1/schema/' endpoint to cache the schema view for 1 hour, improving performance and reducing load on schema generation.

--- elearncore/urls.py
+++ elearncore/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # api path
     path('api-v1/', include('api.urls')),
     # schema & docs
-    path('api-v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api-v1/schema/', cache_page(60 * 60)(SpectacularAPIView.as_view()), name='schema'),
     path('api-v1/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api-v1/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
